74e50ca376ac090b1771c49b8316cc0526373f6d
kandor <nastyakodatskaya@gmail.com>
2026-04-14 14:17:45 +0300

    Update reg.js

--- reg.js
+++ reg.js
@@ -1,1 +1,42 @@
+document.querySelectorAll('.password-input').forEach(input => {
+  const label = input.previousElementSibling; // Получаем предыдущий элемент — наш label
 
+  // Отслеживаем ввод текста
+  input.addEventListener('input', function() {
+    if (this.value.trim() !== '') {
+      label.classList.add('filled'); // Поле заполнено — поднимаем подпись
+    } else {
+      label.classList.remove('filled'); // Поле пустое — опускаем
+    }
+  });
+
+  // Дополнительно отслеживаем фокус
+  input.addEventListener('focus', function() {
+    label.classList.add('focused');
+  });
+  input.addEventListener('blur', function() {
+    label.classList.remove('focused');
+  });
+});
+
+
+document.querySelectorAll('.login-input').forEach(input => {
+  const label = input.previousElementSibling; // Получаем предыдущий элемент — наш label
+
+  // Отслеживаем ввод текста
+  input.addEventListener('input', function() {
+    if (this.value.trim() !== '') {
+      label.classList.add('filled'); // Поле заполнено — поднимаем подпись
+    } else {
+      label.classList.remove('filled'); // Поле пустое — опускаем
+    }
+  });
+
+  // Дополнительно отслеживаем фокус
+  input.addEventListener('focus', function() {
+    label.classList.add('focused');
+  });
+  input.addEventListener('blur', function() {
+    label.classList.remove('focused');
+  });
+});
